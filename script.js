7e8d89dbd8e2fecb25cf1e6a1449c85b90b194e9
niga-odoo <niga@odoo.com>
2022-02-09 14:01:02 +0530

    Add JS file

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log("It's working!!!")
+display = () => {
+    // console.log("It's working from display!!!")
+
+    //Redirect Page
+    window.location.href = '/products';
+}
+
+// // dispatchEvent
+// let btn = document.querySelector('.btn');
+
+// btn.addEventListener('clicks', function () {
+//     alert('Mouse Clicked');
+// });
+
+// let clickEvent = new Event('clicks');
+// btn.dispatchEvent(clickEvent);
